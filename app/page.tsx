44aa9bebe782049625d24d658d7f20600ff5b0cb
JonathanPegaz <pegazjonathan@gmail.com>
2024-08-31 22:00:55 +0200

    finish project

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-"use client";
-import Live from "@/components/Live";
-import Navbar from "@/components/Navbar";
+import dynamic from "next/dynamic";
 
-export default function Page() {
-  return (
-    <main className="h-screen overflow-hidden">
-      <Navbar />
+/**
+ * disable ssr to avoid pre-rendering issues of Next.js
+ *
+ * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
+ */
+const App = dynamic(() => import("./App"), { ssr: false });
 
-      <section className="flex h-full flex-row">
-        <Live />
-      </section>
-    </main>
-  );
-}
+export default App;
