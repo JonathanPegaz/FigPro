2a037f48baf3857e60772ea9c9a4ce52bf709034
JonathanPegaz <pegazjonathan@gmail.com>
2024-08-31 13:42:18 +0200

    liveblocks setup cursor cursorchat & reaction

---
+++ app/Room.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider
+      publicApiKey={
+        "pk_dev_7Aei-XNFN7UXBUnF8Ar1MJgPXn-Rpk0wvY53wb40SoKMTZNvqlysk3sXmuWkq-fQ"
+      }
+    >
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
